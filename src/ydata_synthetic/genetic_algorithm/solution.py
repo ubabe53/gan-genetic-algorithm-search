ff1b78399942054bb923fd10c18259f41b6d3fb3
Umberto <55484855+ubabe53@users.noreply.github.com>
2020-11-26 11:05:37 +0000

    fix(ga_wgan): Fixed mutation, added fid
    fix(wgan): Added frechet inception distance 
    fix(ga): Changed mutation operator
    fix(ga): Converted to minimization problem

--- src/ydata_synthetic/genetic_algorithm/solution.py
+++ src/ydata_synthetic/genetic_algorithm/solution.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
 from numpy import random
-from ydata_synthetic.genetic_algorithm.encoding import encoder
-from ydata_synthetic.synthesizers import WGAN_GP
+from encoding import encoder
+from synthesizers.regular.wgangp.model import WGAN_GP
+from ga_operators import limits_parser
 
 class Solution:
     """
@@ -10,11 +11,11 @@ class Solution:
 
     # Constructor
     # ----------------------------------------------------------------------------------------------
-    def __init__(self):
+    def __init__(self,limits={}):
         self._representation = None
-        self._decoded_repr = None
         self._fitness = None
         self._is_fitness_calculated = False
+        self._limits = limits_parser(limits)
 
         #self._representation = self.representation
 
@@ -32,27 +33,26 @@ class Solution:
         """
         It generates the individual
         """
+        limits = self._limits
         solution_representation = []
-        for _ in range(0, 6):
-            element = int(random.randint(1,6))
-            solution_representation.append(element)
+        for i in range(0, 6):
+            if i in [0,4,5]:
+                max_val = list(limits.values())[i][1]
+                min_val = list(limits.values())[i][0]
+                gene = int(random.randint(min_val,max_val))
+                solution_representation.append(gene)
+            elif i in [1,2,3]:
+                max_val = list(limits.values())[i][1]
+                min_val = list(limits.values())[i][0]
+                gene = random.uniform(min_val, max_val)
+                solution_representation.append(gene)
+
+        solution_representation[1] = round(solution_representation[1], 5)
+        solution_representation[2] = round(solution_representation[2], 2)
+        solution_representation[3] = round(solution_representation[3], 3)
+
         self._representation = solution_representation
 
-
-    # encoder
-    # ----------------------------------------------------------------------------------------------
-    @property
-    def decode(self):
-        if self._decoded_repr is None:
-            self.do_decode()
-        return self._decoded_repr
-
-
-    def do_decode(self):
-        """
-        decode the representation into the phenotype
-        """
-        self._decoded_repr = encoder(self.representation)
 
     # Fitness
     # ----------------------------------------------------------------------------------------------
@@ -61,18 +61,16 @@ class Solution:
     def fitness(self):
         return self._fitness
 
-    def calculate_fitness(self, dimension, data, train_args):  ##TODO Here you gotta implement the WGAN-GP
+    def calculate_fitness(self, dimension, data, train_args):
         #self._fitness = int(random.randint(0,3000,1))
-        parameters = self.decode[:-2]
+        parameters = self.representation[:-2]
         parameters = parameters + dimension
-        n_critic = self.decode[-2:-1][0]
-        weight_gp = self.decode[-1:][0]
+        n_critic = self.representation[-2:-1][0]
+        weight_gp = self.representation[-1:][0]
         model = WGAN_GP(parameters,n_critic,weight_gp)
         self._fitness = model.train(data, train_args)
-    
+
     def set_fitness(self,value):
         self._fitness = value
 
-    def clone(self):
-        return deepcopy(self)
 
